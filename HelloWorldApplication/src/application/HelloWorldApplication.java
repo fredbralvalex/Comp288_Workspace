84f78fd2824fb0558ca6a005d1e5a4a06daf4eb8
Frederico Alexandre <fred.bralvalex@gmail.com>
2018-08-20 10:35:00 -0400

    Revert "Initial commit"
    This reverts commit 996988fc7f94125ce98a9cfc1b148022826ca065.

--- HelloWorldApplication/src/application/HelloWorldApplication.java
+++
@@ -1,8 +0,0 @@
-package application;
-
-public class HelloWorldApplication {
-	public static void main(String[] args) {
-		System.out.println("This is my second application");
-	}
-
-}
--- README.md
+++
@@ -1,1 +0,0 @@
-# Comp288_Workspace
